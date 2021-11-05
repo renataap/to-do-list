365cb58227fec2a83e0591708c2b06e46644aa9c
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-11-04 23:30:46 -0300

    feat: cria model de visualizacao das tarefas

--- models/Task.js
+++ models/Task.js
@@ -1,3 +1,4 @@
+const { ObjectId } = require('mongodb');
 const { getConnection } = require('./connection');
 
 const insertTask = async ({ createDate, title, description, priority, status }) => {
@@ -6,6 +7,24 @@ const insertTask = async ({ createDate, title, description, priority, status }) 
   return { _id: inserted.insertedId, createDate, title, description, priority, status };
 };
 
+const getAllTasks = async () => {
+  const db = await getConnection();
+  const tasks = await db.collection('tasks').find().toArray();
+  return tasks;
+};
+
+const findTaskById = async (id) => {
+  if (!ObjectId.isValid(id)) {
+    return null;
+  }
+
+  const db = await getConnection();
+  const taskId = await db.collection('tasks').findOne(new ObjectId(id));
+  return taskId;
+};
+
 module.exports = {
   insertTask,
+  getAllTasks,
+  findTaskById,
 }
