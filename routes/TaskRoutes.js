fcae60a7e2e3b30c3720b89796a18b84a248e8e0
Renata Andrade <renata@MacBook-Air-de-Renata.local>
2021-11-05 09:09:41 -0300

    feat: cria rota de edicao de tarefa

--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -4,7 +4,7 @@ const taskController = require('../controllers/Task');
 router.get('/tasks', taskController.getAllTasks);
 router.get('/tasks/:id', taskController.findTaskById);
 router.post('/tasks', taskController.insertTask);
-
+router.put('/tasks/:id', taskController.updateTask);
 router.delete('/tasks/:id', taskController.deleteTask);
 
 module.exports = router;
